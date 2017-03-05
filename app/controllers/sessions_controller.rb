ce6d238c85aa417ba7144467ce7ccbc012d3c959
nandukalidindi <nandukalidindi@gmail.com>
2017-03-05 03:46:13 -0500

    Add twitter omniauth gem

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  def setup
+    strategy = request.env['omniauth.strategy']
+    render :text => "Setup complete.", status: 404
+  end
+
+  def create
+    binding.pry
+
+    redirect_to '/', status: 302
+  end
+end
