7a05144b9834f92961cf018d2c775dccea6e8244
nandukalidindi <nandukalidindi@gmail.com>
2017-03-05 01:53:40 -0500

    Install omniauth twitter

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, '836117688741150720-mrNEVa2MjAhtxxW1mmEmoY2fQ4tVYRV', 'Cm58qJiV1TvyCZoGVDnnEU0MO49NehBBn81B7krf41AzW'
+end
