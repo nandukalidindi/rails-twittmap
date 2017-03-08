d048da5002d7ce32e66b866cb70fda56703fe071
nandukalidindi <nandukalidindi@gmail.com>
2017-03-08 02:38:04 -0500

    Modify the schema to match geospatial requirement, lets see what happens

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307083414) do
+ActiveRecord::Schema.define(version: 20170308073325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,11 +25,10 @@ ActiveRecord::Schema.define(version: 20170307083414) do
   end
 
   create_table "tweets", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.string   "text"
-    t.string   "latitude"
-    t.string   "longitude"
+    t.float    "location",   default: [],              array: true
   end
 
 end
