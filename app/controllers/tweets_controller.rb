8b790944102ce0796dfa469ffa7a564e74124d11
nandukalidindi <nandukalidindi@gmail.com>
2017-03-12 14:57:37 -0400

    Show live tweet stream in the tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,23 +1,36 @@
 class TweetsController < ApplicationController
+  include ActionController::Live
   def index
+    response.headers['Content-Type'] = 'text/event-stream'
+
     twitter_app = ConnectedApp.where(name: 'twitter').first
 
-    client = Twitter::Streaming::Client.new do |config|
+    @client = Twitter::Streaming::Client.new do |config|
       config.consumer_key        = "BgB9gfhMrjtSb3ZAy4Zen6u1b"
       config.consumer_secret     = "4vMkasCv3xhs0mRkfEjVePrCQfqoBpxCpgXcGWGTEcE9xywqNR"
       config.access_token        = twitter_app.token
       config.access_token_secret = twitter_app.token_secret
     end
 
-    client.filter(locations: "-180, -90, 180, 90") do |object|
+    filter_tweets
+    response.stream.close
+  end
+
+  def filter_tweets
+    @client.filter(locations: "-180, -90, 180, 90") do |object|
       if object.is_a?(Twitter::Tweet)
         unless object.geo.nil?
-          binding.pry
           location = [object.geo.longitude, object.geo.latitude]
           Tweet.create(text: object.text, location: location)
-          Rails.logger.info "CREATED TWEET #{object.text}"
+          response.stream.write "Tweeted #{object.text} at location #{location}. COUNT: #{Tweet.all.count}\n\n"
+          sleep 0.5
         end
       end
     end
+  rescue EOFError => e
+    response.stream.write "RETRYING FOR CONNECTION. PLEASE WAIT ...\n\n\n\n\n\n"
+    sleep 2
+    retry
   end
+
 end
